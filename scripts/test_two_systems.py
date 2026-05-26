b620281eeefd7f24ee0eeb00b13ee2102b9b36ae
Sai Koukuntla <sai.koukunt@gmail.com>
2026-05-26 14:29:21 -0400

    feat: add random seed control to simulation, restructure project

---
+++ scripts/test_two_systems.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+from plot_utils import plot_C, plot_Fs, plot_X
+from simulations.two_switching_systems import simulate_two_subsystems_no_obs
+
+from decomposed_dynamics.dlds import fit_no_obs, infer_no_obs_state
+
+if __name__ == "__main__":
+    seed = sys.argv[1] if len(sys.argv) > 1 else 0
+    with jax.default_device(jax.devices("cpu")[0]):
+        X, C, F = simulate_two_subsystems_no_obs(3000, [4, 4], [3, 3], 50, seed=seed)
+
+        F_hat = fit_no_obs(X, 15, 200, 10, F_lr_init=5.0)
+        plot_Fs(F)
+        plot_Fs(F_hat)
+
+        plt.show()
--- simulations/src/simulations/two_switching_systems.py
+++ simulations/src/simulations/two_switching_systems.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+import jax.random as jr
 import numpy as np
 import numpy.typing as npt
 from jax import Array
@@ -13,16 +14,19 @@ def simulate_two_subsystems_no_obs(
     num_trials: bool = True,
     min_switch_time: int = 100,
     max_extra_switch_time: int = 300,
+    seed: int = 0,
 ):
     total_latents = sum(num_latents)
     total_motifs = sum(num_motifs)
 
+    keys = jr.split(jr.key(seed), 3)
+
     F = np.zeros((total_motifs, total_latents, total_latents))
     F[: num_motifs[0], : num_latents[0], : num_latents[0]] = create_random_dynamics(
-        num_latents[0], num_motifs[0]
+        num_latents[0], num_motifs[0], keys[0]
     )
     F[num_motifs[0] :, num_latents[0] :, num_latents[0] :] = create_random_dynamics(
-        num_latents[1], num_motifs[1]
+        num_latents[1], num_motifs[1], keys[1]
     )
 
     X, C = _simulate_no_obs_state(
@@ -31,6 +35,7 @@ def simulate_two_subsystems_no_obs(
         num_timepoints,
         num_latents,
         num_motifs,
+        keys[2],
         min_switch_time,
         max_extra_switch_time,
     )
@@ -44,6 +49,7 @@ def _simulate_no_obs_state(
     num_timepoints: int,
     num_latents: npt.NDArray,
     num_motifs: npt.NDArray,
+    key: Array,
     min_switch_time: int = 100,
     max_extra_switch_time: int = 300,
 ):
@@ -54,22 +60,24 @@ def _simulate_no_obs_state(
     C = np.zeros((num_trials, total_motifs, num_timepoints - 1))
 
     for trial in range(num_trials):
+        key, c1_key, c2_key, x_key = jr.split(key, 4)
+
         C[trial][: num_motifs[0]] = generate_switching_c(
             num_motifs[0],
             num_timepoints,
+            c1_key,
             min_switch_time,
             max_extra_switch_time,
-            seed=trial,
         )
         C[trial][num_motifs[0] :] = generate_switching_c(
             num_motifs[1],
             num_timepoints,
+            c2_key,
             min_switch_time,
             max_extra_switch_time,
-            seed=trial + num_trials,
         )
         X[trial] = _simulate_latent_trajectory(
-            C[trial], F, num_latents, num_motifs, num_timepoints
+            C[trial], F, num_latents, num_motifs, num_timepoints, x_key
         )
 
     return X, C
@@ -81,30 +89,32 @@ def _simulate_latent_trajectory(
     num_latents: list[int],
     num_motifs: list[int],
     num_timepoints: int,
+    key: Array,
 ):
-    rng = np.random.default_rng(42)
     total_latents = sum(num_latents)
 
     F_t = np.zeros((total_latents, total_latents))
     X = np.zeros((total_latents, num_timepoints))
 
-    X[:, 0] = rng.standard_normal((total_latents))
+    key, subkey = jr.split(key)
+    X[:, 0] = jr.normal(subkey, (total_latents))
     X[: num_latents[0], 0] /= np.linalg.norm(X[: num_latents[0], 0])
     X[num_latents[0] :, 0] /= np.linalg.norm(X[num_latents[0] :, 0])
 
     for t in range(1, num_timepoints):
         F_t = np.einsum("k, kij -> ij", C[:, t - 1], F)
 
+        key, *subkeys = jr.split(key, 3)
         if (X[: num_latents[0], t - 1] == 0).all() and (
             C[: num_motifs[0], t - 1] != 0
         ).any():
-            X[: num_latents[0], t - 1] = rng.standard_normal((num_latents[0]))
+            X[: num_latents[0], t - 1] = jr.normal(subkeys[0], (num_latents[0]))
             X[: num_latents[0], t - 1] /= np.linalg.norm(X[: num_latents[0], t - 1])
 
         if (X[num_latents[0] :, t - 1] == 0).all() and (
             C[num_motifs[0] :, t - 1] != 0
         ).any():
-            X[num_latents[0] :, t - 1] = rng.standard_normal((num_latents[0]))
+            X[num_latents[0] :, t - 1] = jr.normal(subkeys[1], (num_latents[0]))
             X[num_latents[0] :, t - 1] /= np.linalg.norm(X[num_latents[0] :, t - 1])
 
         X[:, t] = np.einsum("ij, j -> i", F_t, X[:, t - 1])
