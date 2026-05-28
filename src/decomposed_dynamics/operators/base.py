435d8927036cbdcc5bf8266c1f83bb121f6f1804
Sai Koukuntla <sai.koukunt@gmail.com>
2026-05-27 21:30:16 -0400

    feat: add base, linear, and affine operator definitions

---
+++ src/decomposed_dynamics/operators/base.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+import jax.numpy as jnp
+from jax import Array
+
+
+class OperatorParams(ABC):
+    pass
+
+
+class OperatorGroup(ABC):
+    @abstractmethod
+    def initialize_operators(
+        self, key: Array, num_operators: int, input_dim: int, output_dim: int, **kwargs
+    ) -> OperatorParams:
+        raise NotImplementedError
+
+    @abstractmethod
+    def compute_operator_flows(self, operators: OperatorParams, x: Array) -> Array:
+        raise NotImplementedError
+
+    def compute_weighted_flow(self, coeffs: Array, flows: Array) -> Array:
+        return jnp.einsum("...k, ...ki -> i")
+
+    def apply_prox(self, operators: OperatorParams) -> OperatorParams:
+        return operators
