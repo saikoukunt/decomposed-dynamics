dd314c719d86fd01b879b50aec48cd74dd2f6acd
Sai Koukuntla <sai.koukunt@gmail.com>
2026-06-15 20:55:30 -0400

    feat: implement and test model-agnostic `fit_no_obs`

--- scripts/test_two_systems.py
+++ scripts/test_two_systems.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from tqdm import trange
+
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 
 import jax
@@ -11,21 +13,27 @@ from plot_utils import plot_C, plot_Fs, plot_X
 from simulations.two_switching_systems import simulate_two_subsystems_no_obs
 
 from decomposed_dynamics.dynamics_models import (
+    AffineOperatorParams,
+    DecomposedAffineDynamics,
     DecomposedLinearDynamics,
     LinearOperatorParams,
+)
+from decomposed_dynamics.fitting import (
+    compute_dynamics_recon_loss,
+    fit_no_obs,
+    update_operators,
 )
 from decomposed_dynamics.inference import bpdn_inference_no_obs
 
 if __name__ == "__main__":
     seed = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+    lr = 10.0
     with jax.default_device(jax.devices("cpu")[0]):
         X, C, F = simulate_two_subsystems_no_obs(3000, [4, 4], [3, 3], 50, seed=seed)
 
-        model = DecomposedLinearDynamics(num_operators=6, num_latents=8)
-        operators = LinearOperatorParams(F)
-
-        C_hat = bpdn_inference_no_obs(model, operators, X, l1_coeff=0.2)
-        # F_hat = fit_no_obs(X, 15, 200, 20, F_lr_init=10.0, c_l1_coeff=0.2)
-        plot_C(C[13])
-        plot_C(C_hat[13])
+        model = DecomposedLinearDynamics(num_operators=15, num_latents=8)
+        operators = model.initialize_params(jr.key(seed))
+        operators = fit_no_obs(X, model, operators, 200, 20)
+        plot_Fs(F)
+        plot_Fs(operators.F)
         plt.show()
